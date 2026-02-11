94e5de9684c0c5d1654abdc29e83eabb0f66bfad
Diogo Santos <diogofcul@hotmail.com>
2026-02-11 15:11:04 +0000

    Add redirect from root URL to API endpoint in urls.py

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework import routers
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet
 
@@ -28,6 +29,7 @@ router.register(r'workouts', WorkoutViewSet, basename='workout')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
